9984bfd67946ec28f9979a2d14e25d481d26e8cf
TuanLe <tuan.le@exe.com.vn>
2016-05-25 15:22:02 +0700

    web to change locale

--- src/module/Authenticate/configs/route.php
+++ src/module/Authenticate/configs/route.php
@@ -3,7 +3,7 @@
 return [
     'index'      => ['as' => 'auth.index', 'uses' => '\App\Http\Modules\Authenticate\AuthenticateActions@index', 'middleware' => ['web', 'guest'], 'url' => "/login", 'method' => 'get'],
     'postLogin'  => ['uses' => '\App\Http\Controllers\Auth\AuthController@postLogin', 'middleware' => ['web'], 'url' => "/postLogin", 'method' => 'post'],
-    'postLogout' => ['as' => 'auth.logout', 'uses' => '\App\Http\Controllers\Auth\AuthController@getLogout', 'middleware' => null, 'url' => "/auth/logout", 'method' => 'get'],
+    'postLogout' => ['as' => 'auth.logout', 'uses' => '\App\Http\Controllers\Auth\AuthController@getLogout', 'middleware' => ['web'], 'url' => "/auth/logout", 'method' => 'get'],
     'edit'       => null,
     'update'     => null,
     'show'       => null,
