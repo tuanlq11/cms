b83018b253367ab08ff034e3618bc6c13ab458d5
TuanLe <tuan.le@exe.com.vn>
2016-05-25 10:00:02 +0700

    Change middleware auth

--- src/module/Authenticate/configs/route.php
+++ src/module/Authenticate/configs/route.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'index'      => ['as' => 'auth.index', 'uses' => '\App\Http\Modules\Authenticate\AuthenticateActions@index', 'middleware' => null, 'url' => "/login", 'method' => 'get'],
-    'postLogin'  => ['uses' => '\App\Http\Controllers\Auth\AuthController@postLogin', 'middleware' => null, 'url' => "/postLogin", 'method' => 'post'],
+    'index'      => ['as' => 'auth.index', 'uses' => '\App\Http\Modules\Authenticate\AuthenticateActions@index', 'middleware' => ['web', 'guest'], 'url' => "/login", 'method' => 'get'],
+    'postLogin'  => ['uses' => '\App\Http\Controllers\Auth\AuthController@postLogin', 'middleware' => ['web'], 'url' => "/postLogin", 'method' => 'post'],
     'postLogout' => ['as' => 'auth.logout', 'uses' => '\App\Http\Controllers\Auth\AuthController@getLogout', 'middleware' => null, 'url' => "/auth/logout", 'method' => 'get'],
     'edit'       => null,
     'update'     => null,
