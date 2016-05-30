d842b561ffc1c439e6f4f88d952d95701244c39a
TuanLe <tuan.le@exe.com.vn>
2016-05-30 09:44:18 +0700

    Fix dashboard layout

--- src/module/Dashboard/views/index.blade.php
+++ src/module/Dashboard/views/index.blade.php
@@ -1,28 +1,30 @@
-@extends('layout')
+@extends($layout)
 
 @section('title')
-Admin Dashboard
-@endsection
+    Admin Dashboard
+    @endsection
 
-@section('content')
-<!-- Page Heading -->
-<div class="row">
-	<div class="col-lg-12">
-		<div class="page-header">Dashboard&nbsp;<small>Statistic Overview</small></div>
-		<ol class="breadcrumb styled">
-			<li class="home">
-				<a href="{!! route('dashboard.index') !!}"><i class="fa fa-home fa-lg fa-fw"></i></a>
-			</li>
-			<li class="active">Dashboard</li>
-		</ol>
-	</div>
-</div>
-<div class="row">
-	<div class="col-lg-12">
-		<div class="alert alert-info alert-dismissable">
-			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-			<i class="fa fa-info-circle"></i>  <strong>Welcome to Admin Back Office.</strong>
-		</div>
-	</div>
-</div>
+    @section('content')
+            <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="page-header">Dashboard&nbsp;
+                <small>Statistic Overview</small>
+            </div>
+            <ol class="breadcrumb styled">
+                <li class="home">
+                    <a href="{!! route('dashboard.index') !!}"><i class="fa fa-home fa-lg fa-fw"></i></a>
+                </li>
+                <li class="active">Dashboard</li>
+            </ol>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="alert alert-info alert-dismissable">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <i class="fa fa-info-circle"></i> <strong>Welcome to Admin Back Office.</strong>
+            </div>
+        </div>
+    </div>
 @endsection
